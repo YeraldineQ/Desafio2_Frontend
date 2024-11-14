e9ec7d9806685df780ec6f94858ae4c1cf5be26e
YERALDINE QUINTERO <yquintero411@misena.edu.co>
2024-11-13 22:34:56 -0500

    Se agrega método para hacer que el usuario ingrese su correo y password para el login

--- src/app/components/pages/login/loginComponents/login-form/login-form.component.ts
+++ src/app/components/pages/login/loginComponents/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class LoginFormComponent {
 
+  // Datos del usuario
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor( private authService: AuthService) { }
+  // Método para manejar el login
+  onLogin(): void {
+    this.authService.login(this.user.email, this.user.password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        // Aquí puedes almacenar el token en localStorage o manejar la redirección
+        localStorage.setItem('authToken', response.token);
+      },
+      error: (error) => {
+        console.error('Error de autenticación:', error);
+      }
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,13 +6,19 @@ import { Observable } from 'rxjs';
 providedIn: 'root'
 })
 export class AuthService {
-private apiUrl = 'http://localhost:8081/api/auth/register';
+private apiUrl = 'http://localhost:8081/api/auth';
 constructor(private http: HttpClient) {}
 
 // Método para registrar un usuario
 registerUser(userData: any): Observable<any> {
   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  return this.http.post(this.apiUrl, userData, { headers, responseType: 'text'
+  return this.http.post(`${this.apiUrl}/register`, userData, { headers, responseType: 'text'
 }); // <-- Especifica 'text'
 }
+login(email: string, password: string): Observable<any> {
+  const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  const body = { email, password };
+  return this.http.post(`${this.apiUrl}/authenticate`, body, { headers });
+    }
 }
+
